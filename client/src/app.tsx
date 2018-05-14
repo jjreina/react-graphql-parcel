34e51b106cdd61c564b43f743d09d1f4affb648e
jjreina <jreinapascual@gmail.com>
2018-05-14 22:44:00 +0200

    GraphQL on React added fonts google via webfontloader.

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 
 export const App: React.StatelessComponent = () => {
+
     const text: string = 'Hello React plus Typescript';
     const number: number = 6
     return(
-        <h1>{`${text} plus es${number} on Parcel!!`}</h1>
+        <div>
+            <h1>{`${text} plus es${number} on Parcel!!`}</h1>
+        </div>
     )
 } 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
+
 import { App } from './app';
+
+import './index.css';
+
+WebFont.load({
+    google: {
+        families: ['Nunito:600', 'sans-serif']
+    }
+});
 
 ReactDOM.render(
     <App />,
