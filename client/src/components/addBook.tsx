c921d8bf0e76f0927d101914899900b6018f5773
jjreina <jreinapascual@gmail.com>
2018-05-16 16:04:37 +0200

    GraphQL on React added state on form component.

--- client/src/components/addBook.tsx
+++ client/src/components/addBook.tsx
@@ -2,17 +2,29 @@ import * as React from 'react';
 import { graphql } from 'react-apollo';
 
 import { getAuthorsQuery } from '../queries/queries';
+import { SyntheticEvent } from 'react';
 
 interface Props {
     data: any
 }
 
-class AddBook extends React.Component<Props, {}> {
+interface State {
+    name: String,
+    genre: String,
+    authorId: String
+}
+
+class AddBook extends React.Component<Props, State> {
     constructor(props) {
         super(props);
+        this.state = {
+            name: '',
+            genre: '',
+            authorId: ''
+        }
     }
 
-    displayAuthors= () => {
+    displayAuthors = () => {
         const data = this.props.data;
         if (data.loading) {
             return(<option disabled>Loading Authors...</option>);
@@ -25,20 +37,25 @@ class AddBook extends React.Component<Props, {}> {
         }
     }
 
+    submitForm = (e: React.FormEvent<HTMLSelectElement>) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <form id="add-book">
+            <form id="add-book" onSubmit= { this.submitForm.bind(this) }>
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text"/>
+                    <input type="text" onChange= { (e: React.ChangeEvent<HTMLInputElement>) => this.setState({name: e.target.value})}/>
                 </div>
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text"/>
+                    <input type="text" onChange= { (e: React.ChangeEvent<HTMLInputElement>) => this.setState({genre: e.target.value}) }/>
                 </div>
                 <div className="field">
                     <label>Author:</label>
-                    <select>
+                    <select onChange= { (e: React.ChangeEvent<HTMLSelectElement>) => this.setState({authorId: e.target.value})} >
                         <option>Select author</option>
                         {this.displayAuthors()}
                     </select>
